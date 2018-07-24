e5915735985641ac702a3822b7c73e473a0fd3a5
Patrick Jones <patrick.jones@airpr.com>
2018-07-24 14:48:06 -0700

    AP-100: upgrade to AA API 1.4

--- lib/romniture/client.rb
+++ lib/romniture/client.rb
@@ -1,12 +1,8 @@
-
-
 module ROmniture
-  
   class Client
-
     DEFAULT_REPORT_WAIT_TIME = 0.25
-    DEFAULT_API_VERSION = "1.3"
-    
+    DEFAULT_API_VERSION = "1.4"
+
     def initialize(username, shared_secret, environment, options={})
       @username       = username
       @shared_secret  = shared_secret
@@ -48,13 +44,13 @@ module ROmniture
     end
     # insert_request: Inserts data to Catalyst.
     #
-    # data. dict. The field values to insert. See 
+    # data. dict. The field values to insert. See
     #   https://marketing.adobe.com/developer/en_US/documentation/data-insertion/r-supported-tags
     #
     # returns dict. The server response.
 
     def insert_request(data)
-      # Validate the visitorID and convert to hex if necessary 
+      # Validate the visitorID and convert to hex if necessary
       data['visitorID'] = ROmniture::VisitorID.new(data['visitorID']).dec
       response = send_insert_request(data)
 
@@ -75,10 +71,10 @@ module ROmniture
 
       parsed
     end
-    
-    def get_report(method, report_description)      
+
+    def get_report(method, report_description)
       response = send_request(method, report_description)
-      
+
       json = JSON.parse response.body
       if json["status"] == "queued"
         log(Logger::INFO, "Report with ID (" + json["reportID"].to_s + ") queued.  Now fetching report...")
@@ -90,10 +86,10 @@ module ROmniture
     end
 
     # Gets and processes CSV result from Data Warehouse.
-    # 
-    # Designed to be used in a block. 
+    #
+    # Designed to be used in a block.
     # Returns a set of records for each iteratively processed chunk of body.
-    # 
+    #
     # Example:
     #
     # client.get_dw_result(data_url) do |records|
@@ -106,9 +102,9 @@ module ROmniture
 
     def get_dw_result(url, &block)
       generate_nonce
-      
+
       log(Logger::INFO, "Created new nonce: #{@password}")
-      
+
       request = HTTPI::Request.new
 
       request.url = url
@@ -141,28 +137,28 @@ module ROmniture
         if response.code >= 400
           logger.error("Request failed and returned with response code: #{response.code}\n\n#{response.body}")
           w_gz.close
-          raise "Request failed and returned with response code: #{response.code}\n\n#{response.body}" 
+          raise "Request failed and returned with response code: #{response.code}\n\n#{response.body}"
         end
       ensure
         w_gz.close
       end
       wio.string
     end
-    
+
     attr_writer :log
-    
+
     def log?
       @log != false
     end
-    
+
     def logger
       @logger ||= ::Logger.new(STDOUT)
     end
-    
+
     def log_level
       @log_level ||= ::Logger::INFO
     end
-    
+
     def log(*args)
       level = args.first.is_a?(Numeric) || args.first.is_a?(Symbol) ? args.shift : log_level
       logger.log(level, args.join(" ")) if log?
@@ -170,8 +166,8 @@ module ROmniture
 
     # For Trended or Overtime reports, the response from Omniture's API is often deeply nested and difficult to traverse.
     # This function basically denormalizes/flattens the hierarchical structure into a simple array of hashes.
-    # So for example, if the top level element is browser, and the second level is page, and the third level is datetime, 
-    #   this function produces an array of hashes that each have a "browser", "page", and "datetime" keys along with a 
+    # So for example, if the top level element is browser, and the second level is page, and the third level is datetime,
+    #   this function produces an array of hashes that each have a "browser", "page", and "datetime" keys along with a
     #   key for each metric.
     def flatten_response(resp)
       @flattened = []
@@ -182,16 +178,16 @@ module ROmniture
       #For some reason OMTR doesn't list the datetime in the elements list if it is the top-level element, so add it in.
       datetime_index = elements.index{|e| e == "datetime"}
       elements.unshift("datetime") if datetime_index.nil?
-      
-      flatten_report(report['data'], elements, metrics) 
+
+      flatten_report(report['data'], elements, metrics)
       @flattened
     end
-        
+
     private
     def flatten_report( data, elements, metrics, current_level = 1, result_row = nil)
       data.each do |current_node|
         if current_level == 1 then result_row = Hash.new else result_row = result_row.clone end
-       
+
         #Get the element at this level and store it in our result row, so this will end up with something like
         # { "browser" => "Google Chrome 25.0" } or { "datetime" => "Fri. 1 Mar. 2013" }
         element_name = elements[current_level-1]
@@ -218,7 +214,7 @@ module ROmniture
 
     ##
     # Parses the Data Warehouse CSV file into a list.
-    # 
+    #
     # param: csv. str. Properly formatted CSV as a string.
     # returns: array.
     #
@@ -242,9 +238,9 @@ module ROmniture
 
     def send_insert_request(data)
       generate_nonce
-      
+
       log(Logger::INFO, "Created new nonce: #{@password}")
-      
+
       request = HTTPI::Request.new
 
       data['scXmlVer'] = "1.0"
@@ -264,20 +260,20 @@ module ROmniture
 
       if response.code >= 400
         log(Logger::ERROR, "Request failed and returned with response code: #{response.code}\n\n#{response.body}")
-        raise "Request failed and returned with response code: #{response.code}\n\n#{response.body}" 
+        raise "Request failed and returned with response code: #{response.code}\n\n#{response.body}"
       end
 
       log(Logger::INFO, "Server responded with response code #{response.code}.")
 
       response
     end
-    
+
     def send_request(method, data)
       log(Logger::INFO, "Requesting #{method}...")
       generate_nonce
-      
+
       log(Logger::INFO, "Created new nonce: #{@password}")
-      
+
       request = HTTPI::Request.new
 
       if @verify_mode
@@ -289,17 +285,17 @@ module ROmniture
       request.body = data.to_json
 
       response = HTTPI.post(request)
-      
+
       if response.code >= 400
         log(Logger::ERROR, "Request failed and returned with response code: #{response.code}\n\n#{response.body}")
-        raise "Request failed and returned with response code: #{response.code}\n\n#{response.body}" 
+        raise "Request failed and returned with response code: #{response.code}\n\n#{response.body}"
       end
-      
+
       log(Logger::INFO, "Server responded with response code #{response.code}.")
-      
+
       response
     end
-    
+
     def generate_nonce
       @nonce          = Digest::MD5.new.hexdigest(rand().to_s)
       case @api_version
@@ -313,15 +309,15 @@ module ROmniture
       combined_string = @nonce + @created + @shared_secret
       sha1_string     = Digest::SHA1.new.hexdigest(combined_string)
       @password       = Base64.encode64(sha1_string).to_s.chomp("\n")
-    end    
+    end
 
-    def request_headers 
+    def request_headers
       {
         "X-WSSE" => "UsernameToken Username=\"#{@username}\", PasswordDigest=\"#{@password}\", Nonce=\"#{@nonce}\", Created=\"#{@created}\"",
         'Content-Type' => 'application/json'   #Added by ROB on 2013-08-22 because the Adobe Social API seems to require this be set
       }
     end
-    
+
     def get_queued_report(report_id)
       done = false
       error = false
@@ -332,30 +328,30 @@ module ROmniture
       begin
         response = send_request("Report.GetStatus", {"reportID" => "#{report_id}"})
         log(Logger::INFO, "Checking on status of report #{report_id}...")
-        
+
         json = JSON.parse(response.body)
         status = json["status"]
-        
+
         if status == "done"
           done = true
         elsif status == "failed"
           error = true
         end
-        
+
         sleep @wait_time if !done && !error
       end while !done && !error
-      
+
       if error
         msg = "Unable to get data for report #{report_id}.  Status: #{status}.  Error Code: #{json["error_code"]}.  #{json["error_msg"]}."
         log(Logger::ERROR, msg)
         raise ROmniture::Exceptions::OmnitureReportException.new(json), msg
       end
-            
+
       response = send_request("Report.GetReport", {"reportID" => "#{report_id}"})
 
       end_time = Time.now
       log(Logger::INFO, "Report with ID #{report_id} has finished processing in #{((end_time - start_time)*1000).to_i} ms")
-      
+
       JSON.parse(response.body)
     end
   end
