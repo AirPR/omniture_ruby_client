7e2f4f52d9cc0e697210127afa184149a9ad8020
Snehal <snehal.shinde@velotio.com>
2020-09-17 18:49:34 +0530

    log events

--- lib/romniture/report_response.rb
+++ lib/romniture/report_response.rb
@@ -82,8 +82,8 @@ module ROmniture
           metrics.each do |metric|
             matches = /\((event[0-9]+)\)/.match(metric["id"])
             evar_matches = /\((evar[0-9]+)\)/.match(metric["id"])
-            @logger.info(matches)
-            @logger.info(evar_matches)
+            @logger.info("matches #{matches}")
+            @logger.info("evar_matches #{evar_matches}")
             if (matches and matches.length) || (evar_matches and evar_matches.length)
               @csv_header << "\"#{metric["name"]}(#{metric["id"]})\""
             else
